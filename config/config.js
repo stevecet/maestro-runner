0de38b150e765b10ca34fbee1df1d4420613cf35
Steve Veceto <steve.veceto@cm.maviance.com>
2025-12-26 14:26:04 +0100

    Changed the image container

---
+++ config/config.js
@@ -0,0 +1,3 @@
+output.env = {
+    timeout: 30000 // 30 seconds in milliseconds
+}
---
+++ data/kyc/data.js
@@ -0,0 +1,15 @@
+// data/kyc/data.js
+const kyc_data = {
+  firstName: "Test",
+  lastName: "Mobile",
+  gender: "Male",
+  dateOfBirth: "17/02/1990",
+  nationality: "Cameroon",
+  documentType: "CNI",
+  IdNumber: "216500027332",
+  dateOfIssue: "15/10/2019",
+  mainPhone: "653154187",
+  whatsappPhone: "653154187",
+};
+
+output.kyc_data = kyc_data;
